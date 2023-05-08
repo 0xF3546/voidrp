d43d7b6f63952c32b522edb5cae1d93f498bce79
Mayson1337 <hampelmann361@gmail.com>
2023-05-08 02:53:30 +0200

    add json

--- src/main/java/de/polo/void_roleplay/Utils/Housing.java
+++ src/main/java/de/polo/void_roleplay/Utils/Housing.java
@@ -1,16 +1,14 @@
 package de.polo.void_roleplay.Utils;
 
-import de.polo.void_roleplay.DataStorage.FactionData;
 import de.polo.void_roleplay.DataStorage.HouseData;
 import de.polo.void_roleplay.MySQl.MySQL;
 import org.bukkit.entity.Player;
+import org.json.JSONArray;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class Housing {
     public static Map<Integer, HouseData> houseDataMap = new HashMap<>();
@@ -23,6 +21,12 @@ public class Housing {
             houseData.setOwner(locs.getString(2));
             houseData.setNumber(locs.getInt(3));
             houseData.setPrice(locs.getInt(4));
+            JSONArray array = new JSONArray(locs.getString(5));
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < array.length(); i++) {
+                list.add(array.getString(i));
+            }
+            houseData.setRenter(list);
             houseDataMap.put(locs.getInt(3), houseData);
         }
     }
@@ -37,7 +41,6 @@ public class Housing {
     }
 
     public static boolean canPlayerInteract(Player player, int number) {
-        System.out.println("number bei interact: " + number);
         HouseData houseData = houseDataMap.get(number);
         if (!Objects.equals(houseData.getOwner(), player.getUniqueId().toString())) {
             for (int i = 0; i < houseData.getRenter().size(); i++) {
@@ -49,5 +52,15 @@ public class Housing {
             return true;
         }
         return false;
+    }
+    public static void updateRenter(int number) {
+        HouseData houseData = houseDataMap.get(number);
+        try {
+            Statement statement = MySQL.getStatement();
+            JSONArray array = new JSONArray(houseData.getRenter());
+            statement.executeUpdate("UPDATE `housing` SET `renter` = '" + array + "'");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
