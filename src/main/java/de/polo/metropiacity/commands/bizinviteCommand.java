b2ec63896adfb5dac1dbeb6bce282d7f588f8e6b
Mayson1337 <hampelmann361@gmail.com>
2023-07-01 07:27:26 +0200

    bizinviteCommand

---
+++ src/main/java/de/polo/metropiacity/commands/bizinviteCommand.java
@@ -0,0 +1,68 @@
+package de.polo.metropiacity.commands;
+
+import de.polo.metropiacity.DataStorage.BusinessData;
+import de.polo.metropiacity.DataStorage.FactionData;
+import de.polo.metropiacity.DataStorage.PlayerData;
+import de.polo.metropiacity.Main;
+import de.polo.metropiacity.Utils.BusinessManager;
+import de.polo.metropiacity.Utils.FactionManager;
+import de.polo.metropiacity.Utils.PlayerManager;
+import de.polo.metropiacity.Utils.VertragUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class bizinviteCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
+        PlayerData playerData = PlayerManager.playerDataMap.get(player.getUniqueId().toString());
+        if (playerData.getBusiness() != null) {
+            String playerfac = playerData.getBusiness();
+            BusinessData businessData = BusinessManager.businessDataMap.get(playerfac);
+            if (playerData.getBusiness_grade() >= 4) {
+                if (args.length >= 1) {
+                    Player targetplayer = Bukkit.getPlayer(args[0]);
+                    assert targetplayer != null;
+                    if (player.getLocation().distance(targetplayer.getLocation()) <= 5) {
+                        if (PlayerManager.playerDataMap.get(targetplayer.getUniqueId().toString()).getBusiness() == null) {
+                            if (BusinessManager.getMemberCount(playerfac) < businessData.getMaxMember()) {
+                                try {
+                                    if (VertragUtil.setVertrag(player, targetplayer, "business_invite", playerfac)) {
+                                        player.sendMessage("§8[§6Business§8] §7" + targetplayer.getName() + " wurde in das Business §aeingeladen§7.");
+                                        targetplayer.sendMessage("§6" + player.getName() + " hat dich in das Business §e" + playerfac + "§6 eingeladen.");
+                                        VertragUtil.sendInfoMessage(targetplayer);
+                                        PlayerData tplayerData = PlayerManager.playerDataMap.get(targetplayer.getUniqueId().toString());
+                                        //tplayerData.setVariable("current_inventory", "faction_invite");
+                                    } else {
+                                        player.sendMessage("§8[§6Business§8]§8 §7" + targetplayer.getName() + " hat noch einen Vertrag offen.");
+                                    }
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            } else {
+                                player.sendMessage(Main.error + "Dein Business ist voll!");
+                            }
+                        } else {
+                            player.sendMessage("§8[§6Business§8] §c" + targetplayer.getName() + "§7 ist bereits in einem Business.");
+                        }
+                    } else {
+                        player.sendMessage(Main.error + targetplayer.getName() + " ist nicht in deiner nähe.");
+                    }
+                } else {
+                    player.sendMessage(Main.error + "Syntax-Fehler: /bizinvite [Spieler]");
+                }
+            } else {
+                player.sendMessage(Main.error_nopermission);
+            }
+        } else {
+            player.sendMessage(Main.error + "Du bist in keinem Business");
+        }
+        return false;
+    }
+}
