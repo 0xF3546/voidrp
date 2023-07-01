841562c77478744a08b9c6839f97570f9f37bd0a
Mayson1337 <hampelmann361@gmail.com>
2023-07-01 07:27:34 +0200

    fix player ban

--- src/main/java/de/polo/metropiacity/commands/banCommand.java
+++ src/main/java/de/polo/metropiacity/commands/banCommand.java
@@ -35,7 +35,7 @@ public class banCommand implements CommandExecutor {
                     }
                     for (Player players : Bukkit.getOnlinePlayers()) {
                         if (players.getName().equalsIgnoreCase(args[1])) {
-                            players.kickPlayer("§8• §6§lMetropiaCity §8•\n\n§cDu wurdest vom Server gebannt.\nGrund§8:§7 " + banreason + "\n\n§8• §6§MetropiaCity §8•");
+                            players.kickPlayer("§8• §6§lMetropiaCity §8•\n\n§cDu wurdest vom Server gebannt.\nGrund§8:§7 " + banreason + "\n\n§8• §6§lMetropiaCity §8•");
                         }
                     }
                     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -50,37 +50,32 @@ public class banCommand implements CommandExecutor {
                                 calendar.add(Calendar.HOUR, Integer.parseInt(args[2].replace("h", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + uuid + "', '" + args[1] + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + uuid + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + args[1] + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("d")) {
                                 calendar.add(Calendar.DAY_OF_YEAR, Integer.parseInt(args[2].replace("d", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + uuid + "', '" + args[1] + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + uuid + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + args[1] + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("m")) {
                                 calendar.add(Calendar.MONTH, Integer.parseInt(args[2].replace("m", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + uuid + "', '" + args[1] + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + uuid + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + args[1] + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("s")) {
                                 calendar.add(Calendar.SECOND, Integer.parseInt(args[2].replace("s", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + uuid + "', '" + args[1] + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + uuid + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + args[1] + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("y")) {
                                 calendar.add(Calendar.YEAR, Integer.parseInt(args[2].replace("y", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + uuid + "', '" + args[1] + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + uuid + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + args[1] + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             }
                         }
                     } catch (SQLException e) {
@@ -108,37 +103,32 @@ public class banCommand implements CommandExecutor {
                                 calendar.add(Calendar.HOUR, Integer.parseInt(args[2].replace("h", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + args[1] + "', '" + playername + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + args[1] + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + playername + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("d")) {
                                 calendar.add(Calendar.DAY_OF_YEAR, Integer.parseInt(args[2].replace("d", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + args[1] + "', '" + playername + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + args[1] + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + playername + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("m")) {
                                 calendar.add(Calendar.MONTH, Integer.parseInt(args[2].replace("m", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + args[1] + "', '" + playername + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + args[1] + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + playername + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("s")) {
                                 calendar.add(Calendar.SECOND, Integer.parseInt(args[2].replace("s", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + args[1] + "', '" + playername + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + args[1] + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + playername + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             } else if (args[2].contains("y")) {
                                 calendar.add(Calendar.YEAR, Integer.parseInt(args[2].replace("y", "")));
                                 Date newDate = calendar.getTime();
                                 statement.execute("INSERT INTO `player_bans` (`uuid`, `name`, `reason`, `gebannt`) VALUES ('" + args[1] + "', '" + playername + "', '" + banreason + "', '" + new Date() +"')");
-                                statement.executeUpdate("UPDATE `players` SET `isBanned` = true WHERE `uuid` = '" + args[1] + "'");
                                 Bukkit.broadcastMessage("§c" + playerData.getRang() + " " + player.getName() + " hat " + playername + " gebannt. Grund:" + banreason);
-                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6www.voidroleplay.de/forum/\n\n§8• §6§lMetropiaCity §8•", newDate, null);
+                                Bukkit.getBanList(BanList.Type.NAME).addBan(offlinePlayer.getName(), "§8• §6§lMetropiaCity §8•\n\n§cDu bist vom Server gebannt.\nGrund§8:§7 " + banreason + "\n§cBan läuft ab§8:§7 " + newDate + "\n\n§cEntbannung unter\n§6entbannung@metropiacity.de\n\n§8• §6§lMetropiaCity §8•", newDate, null);
                             }
                         }
                     } catch (SQLException e) {
