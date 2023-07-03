94399c0d26ae0a537ec9202b15b6b609a28a5a2c
Mayson1337 <hampelmann361@gmail.com>
2023-07-03 06:31:25 +0200

    better memberCommand

--- src/main/java/de/polo/metropiacity/commands/memberCommand.java
+++ src/main/java/de/polo/metropiacity/commands/memberCommand.java
@@ -14,7 +14,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.UUID;
+import java.util.*;
 
 public class memberCommand implements CommandExecutor {
     @Override
@@ -24,14 +24,33 @@ public class memberCommand implements CommandExecutor {
         if (playerData.getFaction() != null && !playerData.getFaction().equals("Zivilist")) {
             FactionData factionData = FactionManager.factionDataMap.get(playerData.getFaction());
             player.sendMessage("§7   ===§8[§" + factionData.getPrimaryColor() + factionData.getFullname() + "§8]§7===");
+            Map<Integer, List<OfflinePlayer>> sort = new HashMap<>();
             for (FactionPlayerData factionPlayerData : ServerManager.factionPlayerDataMap.values()) {
                 if (factionPlayerData.getFaction().equals(playerData.getFaction())) {
                     OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(factionPlayerData.getUuid()));
-                    if (offlinePlayer.isOnline()) {
-                        player.sendMessage("§8 ➥ §a" + offlinePlayer.getName() + "§8 | §aRang " + factionPlayerData.getFaction_grade());
+                    if (sort.containsKey(factionPlayerData.getFaction_grade())) {
+                        sort.get(factionPlayerData.getFaction_grade()).add(offlinePlayer);
+                        System.out.println(factionPlayerData.getFaction_grade() + " | " + offlinePlayer.getName());
                     } else {
-                        player.sendMessage("§8 ➥ §c" + offlinePlayer.getName() + "§8 | §cRang " + factionPlayerData.getFaction_grade());
+                        sort.put(factionPlayerData.getFaction_grade(), new ArrayList<>());
+                        sort.get(factionPlayerData.getFaction_grade()).add(offlinePlayer);
                     }
+                }
+            }
+            for (int value = 8; value >= 0; value--) {
+                String message = "§8 » §" + factionData.getSecondaryColor() + FactionManager.getRankName(playerData.getFaction(), value) + " - " + FactionManager.getPaydayFromFaction(playerData.getFaction(), value) + "$/PayDay";
+                player.sendMessage(message);
+                if (sort.get(value) != null) {
+                    for (int i = 0; i < sort.get(value).size(); i++) {
+                        OfflinePlayer offlinePlayer = sort.get(value).get(i);
+
+                        if (offlinePlayer.isOnline()) {
+                            player.sendMessage("§8 → §a" + offlinePlayer.getName());
+                        } else {
+                            player.sendMessage("§8 → §c" + offlinePlayer.getName());
+                        }
+                    }
+                    sort.remove(value);
                 }
             }
             player.sendMessage(" ");
