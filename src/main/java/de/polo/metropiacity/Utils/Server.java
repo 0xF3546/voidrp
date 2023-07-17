e9f93d850c090e9438e309255c6421cab467cf4c
Mayson1337 <hampelmann361@gmail.com>
2023-07-17 21:56:05 +0200

    change ServerFunctions

---
+++ src/main/java/de/polo/metropiacity/Utils/Server.java
@@ -0,0 +1,46 @@
+package de.polo.metropiacity.Utils;
+
+import de.polo.metropiacity.DataStorage.PlayerData;
+import de.polo.metropiacity.Main;
+import de.polo.metropiacity.MySQl.MySQL;
+import de.polo.metropiacity.PlayerUtils.ChatUtils;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Server {
+    public interface TeamSpeak {
+    }
+    public interface Faction {
+
+    }
+    public interface Utils {
+        static void kissPlayer(Player player, Player targetplayer) {
+            if (player.getLocation().distance(targetplayer.getLocation()) < 5) {
+                ChatUtils.sendMeMessageAtPlayer(player, "§o" + player.getName() + " gibt " + targetplayer.getName() + " einen Kuss.");
+                player.spawnParticle(Particle.HEART, player.getLocation().add(0, 2, 0), 1);
+                targetplayer.spawnParticle(Particle.HEART, targetplayer.getLocation().add(0, 2, 0), 1);
+            } else {
+                player.sendMessage(Main.error + targetplayer.getName() + " ist nicht in deiner nähe.");
+            }
+        }
+    }
+    public interface Economy {
+        interface farming {
+
+        }
+        interface jobs {
+
+        }
+        interface user {
+            static void addMoney(Player player, int amount) {
+            }
+            static void removeMoney(Player player, int amount) {
+
+            }
+        }
+    }
+}
