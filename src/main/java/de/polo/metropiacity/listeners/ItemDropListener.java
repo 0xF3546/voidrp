fba611bf3f131899a341e483581c7ed54c7deb60
Mayson1337 <hampelmann361@gmail.com>
2023-07-21 04:36:55 +0200

    fix weapon reload

--- src/main/java/de/polo/metropiacity/listeners/ItemDropListener.java
+++ src/main/java/de/polo/metropiacity/listeners/ItemDropListener.java
@@ -1,10 +1,12 @@
 package de.polo.metropiacity.listeners;
 
+import de.polo.metropiacity.Main;
 import de.polo.metropiacity.dataStorage.WeaponData;
 import de.polo.metropiacity.utils.Weapons;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class ItemDropListener implements Listener {
     @EventHandler
@@ -12,7 +14,12 @@ public class ItemDropListener implements Listener {
         WeaponData weaponData = Weapons.weaponDataMap.get(event.getItemDrop().getItemStack().getType());
         if (weaponData != null) {
             event.setCancelled(true);
-            Weapons.reloadWeapon( event.getPlayer(), event.getItemDrop().getItemStack());
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    Weapons.reload(event.getPlayer(), event.getItemDrop().getItemStack());
+                }
+            }.runTaskLater(Main.getInstance(), (long) (weaponData.getReloadDuration() * 2));
         }
     }
 }
