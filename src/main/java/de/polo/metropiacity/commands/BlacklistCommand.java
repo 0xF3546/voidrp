9c9eabf6dd0777af8adc0a191b239dde94c8f7b7
Mayson1337 <hampelmann361@gmail.com>
2023-07-21 04:07:50 +0200

    improve blacklist command

--- src/main/java/de/polo/metropiacity/commands/BlacklistCommand.java
+++ src/main/java/de/polo/metropiacity/commands/BlacklistCommand.java
@@ -27,115 +27,121 @@ public class BlacklistCommand implements CommandExecutor, TabCompleter {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
         PlayerData playerData = PlayerManager.playerDataMap.get(player.getUniqueId().toString());
-        if (playerData.getFaction() != null && playerData.getFaction() != "Zivilist") {
-            FactionData factionData = FactionManager.factionDataMap.get(playerData.getFaction());
-            if (factionData.hasBlacklist()) {
-                if (args.length == 0) {
-                    player.sendMessage("§7   ===§8[§cBlacklist§8]§7===");
-                    player.sendMessage("§8 ➥ §" + factionData.getPrimaryColor() + factionData.getFullname());
-                    player.sendMessage(" ");
+        if (playerData.getFaction() == null) {
+            player.sendMessage(Main.error_nopermission);
+            return false;
+        }
+        FactionData factionData = FactionManager.factionDataMap.get(playerData.getFaction());
+        if (!factionData.hasBlacklist()) {
+            player.sendMessage(Main.error + "Deine Fraktion hat keine Blacklist.");
+            return false;
+        }
+        if (args.length == 0) {
+            player.sendMessage("§7   ===§8[§cBlacklist§8]§7===");
+            player.sendMessage("§8 ➥ §" + factionData.getPrimaryColor() + factionData.getFullname());
+            player.sendMessage(" ");
+            for (BlacklistData blacklistData : FactionManager.blacklistDataMap.values()) {
+                if (blacklistData.getFaction().equals(factionData.getName())) {
+                    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(blacklistData.getUuid()));
+                    player.sendMessage("§8 ➥ §e" + offlinePlayer.getName() + "§8 | §e" + blacklistData.getPrice() + "$ §8 | §e" + blacklistData.getKills() + " Tode §8| §e" + blacklistData.getReason() + " §8|§e " + blacklistData.getDate());
+                }
+            }
+            return false;
+        } else {
+            if (!(args.length >= 2)) {
+                player.sendMessage(Main.error + "Syntax-Fehler: /blacklist [add/remove] [Spieler] [Kills] [Preis] [Grund]");
+                return false;
+            }
+            if (args[0].equalsIgnoreCase("add")) {
+                if (args.length >= 4) {
+                    Player player1 = Bukkit.getPlayer(args[1]);
+                    if (player1 == null) {
+                        player.sendMessage(Main.error + args[0] + " ist nicht online.");
+                        return false;
+                    }
+                    if (playerData.getFactionGrade() < 3) {
+                        player.sendMessage(Main.error + "Dieser Befehl ist erst ab Rang 3+ verfübar.");
+                        return false;
+                    }
+                    boolean canDo = true;
                     for (BlacklistData blacklistData : FactionManager.blacklistDataMap.values()) {
-                        if (blacklistData.getFaction().equals(factionData.getName())) {
-                            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(blacklistData.getUuid()));
-                            player.sendMessage("§8 ➥ §e" + offlinePlayer.getName() + "§8 | §e" + blacklistData.getPrice() + "$ §8 | §e" + blacklistData.getKills() + " Tode §8| §e" + blacklistData.getReason() + " §8|§e " + blacklistData.getDate());
+                        if (blacklistData.getFaction().equals(playerData.getFaction())) {
+                            if (Objects.equals(blacklistData.getUuid(), player1.getUniqueId().toString())) {
+                                canDo = false;
+                                break;
+                            }
                         }
                     }
-                } else {
-                    if (args.length >= 1) {
-                        if (args.length >= 4) {
-                            if (args[0].equalsIgnoreCase("add")) {
-                                Player player1 = Bukkit.getPlayer(args[1]);
-                                if (player1.isOnline()) {
-                                    if (playerData.getFactionGrade() >= 3) {
-                                        boolean canDo = true;
-                                        for (BlacklistData blacklistData : FactionManager.blacklistDataMap.values()) {
-                                            if (blacklistData.getFaction().equals(playerData.getFaction())) {
-                                                if (Objects.equals(blacklistData.getUuid(), player1.getUniqueId().toString())) {
-                                                    canDo = false;
-                                                    break;
-                                                }
-                                            }
-                                        }
-                                        if (canDo) {
-                                            int price = Integer.parseInt(args[2]);
-                                            int kills = Integer.parseInt(args[3]);
-                                            String reason = "";
-                                            for (int i = 4; i < args.length; i++) {
-                                                reason = reason + " " + args[i];
-                                            }
-                                            try {
-                                                SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyy '|' HH:mm:ss ");
-                                                Date date = new Date();
-                                                String newDate = formatter.format(date);
-                                                Statement statement = MySQL.getStatement();
-                                                statement.execute("INSERT INTO `blacklist` (`uuid`, `faction`, `kills`, `price`, `date`, `reason`) VALUES ('" + player1.getUniqueId() + "', '" + factionData.getName() + "', " + kills + ", " + price + ", '" + newDate + "', '" + reason + "')");
-                                                ResultSet checkId = statement.executeQuery("SELECT `id` FROM `blacklist` WHERE `uuid` = '" + player1.getUniqueId() + "' AND `date` = '" + newDate + "'");
-                                                if (checkId.next()) {
-                                                    BlacklistData blacklistData = new BlacklistData();
-                                                    blacklistData.setDate(newDate);
-                                                    blacklistData.setPrice(price);
-                                                    blacklistData.setReason(reason);
-                                                    blacklistData.setKills(kills);
-                                                    blacklistData.setFaction(factionData.getName());
-                                                    blacklistData.setId(checkId.getInt(1));
-                                                    blacklistData.setUuid(player1.getUniqueId().toString());
-                                                    FactionManager.blacklistDataMap.put(checkId.getInt(1), blacklistData);
-                                                }
-                                                FactionManager.sendMessageToFaction(factionData.getName(), FactionManager.getPlayerFactionRankName(player) + " " + player.getName() + " hat " + player1.getName() + " auf die Blacklist gesetzt.");
-                                                player1.sendMessage("§8[§cBlacklist§8]§c Du wurdest auf die Blacklist von " + factionData.getFullname() + " gesetzt.");
-                                                player1.sendMessage("§8[§cBlacklist§8]§c " + kills + " Kills §8| §c" + price + "$§8 | §c" + reason);
-                                            } catch (SQLException e) {
-                                                player.sendMessage(Main.error + "Bitte versuche es später erneut.");
-                                                throw new RuntimeException(e);
-                                            }
-                                        } else {
-                                            player.sendMessage(Main.error + player1.getName() + " ist bereits auf der Blacklist.");
-                                        }
-                                    } else {
-                                        player.sendMessage(Main.error + "Dieser Befehl ist erst ab Rang 3+ verfübar.");
-                                    }
-                                } else {
-                                    player.sendMessage(Main.error + args[0] + " ist nicht online.");
-                                }
-                            }
-                        } else if (args[0].equalsIgnoreCase("remove")) {
-                            Player player1 = Bukkit.getPlayer(args[1]);
-                            if (player1.isOnline()) {
-                                if (playerData.getFactionGrade() >= 5) {
-                                    boolean canDo = false;
-                                    for (BlacklistData blacklistData : FactionManager.blacklistDataMap.values()) {
-                                        if (blacklistData.getUuid().equals(player1.getUniqueId().toString()) && blacklistData.getFaction().equals(factionData.getName())) {
-                                            canDo = true;
-
-                                            try {
-                                                Statement statement = MySQL.getStatement();
-                                                statement.execute("DELETE FROM `blacklist` WHERE `id` = " + blacklistData.getId());
-                                                FactionManager.sendMessageToFaction(factionData.getName(), "§c" + FactionManager.getPlayerFactionRankName(player) + " " + player.getName() + " hat " + player1.getName() + " von der Blacklist gelöscht.");
-                                                FactionManager.blacklistDataMap.remove(blacklistData.getId());
-                                                player1.sendMessage("§8[§cBlacklist§8]§7 " + player.getName() + " hat dich von der Blacklist der " + factionData.getFullname() + " gelöscht");
-                                            } catch (SQLException e) {
-                                                throw new RuntimeException(e);
-                                            }
-                                        }
-                                    }
-                                    if (!canDo)
-                                        player.sendMessage(Main.error + player1.getName() + " ist nicht auf der Blacklist.");
-                                } else {
-                                    player.sendMessage(Main.error + "Dieser Befehl ist erst ab Rang 5+ verfübar.");
-                                }
-                            }
-                        } else {
-                            player.sendMessage(Main.error + "Syntax-Fehler: /blacklist add [Spieler] [Preis] [Kills] [Grund]");
+                    if (!canDo) {
+                        player.sendMessage(Main.error + player1.getName() + " ist bereits auf der Blacklist.");
+                        return false;
+                    }
+                    int price = Integer.parseInt(args[3]);
+                    int kills = Integer.parseInt(args[2]);
+                    String reason = "";
+                    for (int i = 4; i < args.length; i++) {
+                        reason = reason + " " + args[i];
+                    }
+                    try {
+                        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyy '|' HH:mm:ss ");
+                        Date date = new Date();
+                        String newDate = formatter.format(date);
+                        Statement statement = MySQL.getStatement();
+                        statement.execute("INSERT INTO `blacklist` (`uuid`, `faction`, `kills`, `price`, `date`, `reason`) VALUES ('" + player1.getUniqueId() + "', '" + factionData.getName() + "', " + kills + ", " + price + ", '" + newDate + "', '" + reason + "')");
+                        ResultSet checkId = statement.executeQuery("SELECT `id` FROM `blacklist` WHERE `uuid` = '" + player1.getUniqueId() + "' AND `date` = '" + newDate + "'");
+                        if (checkId.next()) {
+                            BlacklistData blacklistData = new BlacklistData();
+                            blacklistData.setDate(newDate);
+                            blacklistData.setPrice(price);
+                            blacklistData.setReason(reason);
+                            blacklistData.setKills(kills);
+                            blacklistData.setFaction(factionData.getName());
+                            blacklistData.setId(checkId.getInt(1));
+                            blacklistData.setUuid(player1.getUniqueId().toString());
+                            FactionManager.blacklistDataMap.put(checkId.getInt(1), blacklistData);
                         }
-                    } else {
-                        player.sendMessage(Main.error + "Syntax-Fehler: /blacklist [add/remove] [Spieler] [Preis] [Kills] [Grund]");
+                        FactionManager.sendMessageToFaction(factionData.getName(), FactionManager.getPlayerFactionRankName(player) + " " + player.getName() + " hat " + player1.getName() + " auf die Blacklist gesetzt.");
+                        player1.sendMessage("§8[§cBlacklist§8]§c Du wurdest auf die Blacklist von " + factionData.getFullname() + " gesetzt.");
+                        player1.sendMessage("§8[§cBlacklist§8]§c " + kills + " Kills §8| §c" + price + "$§8 | §c" + reason);
+                    } catch (SQLException e) {
+                        player.sendMessage(Main.error + "Bitte versuche es später erneut.");
+                        throw new RuntimeException(e);
                     }
                 }
+            } else if (args[0].equalsIgnoreCase("remove")) {
+                Player player1 = Bukkit.getPlayer(args[1]);
+                if (!(args.length <= 2)) {
+                    return false;
+                }
+                if (player1 == null) {
+                    player.sendMessage(Main.error + args[0] + " ist nicht online.");
+                    return false;
+                }
+                if (playerData.getFactionGrade() < 5) {
+                    player.sendMessage(Main.error + "Dieser Befehl ist erst ab Rang 5+ verfübar.");
+                    return false;
+                }
+                boolean canDo = false;
+                for (BlacklistData blacklistData : FactionManager.blacklistDataMap.values()) {
+                    if (blacklistData.getUuid().equals(player1.getUniqueId().toString()) && blacklistData.getFaction().equals(factionData.getName())) {
+                        canDo = true;
+
+                        try {
+                            Statement statement = MySQL.getStatement();
+                            statement.execute("DELETE FROM `blacklist` WHERE `id` = " + blacklistData.getId());
+                            FactionManager.sendMessageToFaction(factionData.getName(), "§c" + FactionManager.getPlayerFactionRankName(player) + " " + player.getName() + " hat " + player1.getName() + " von der Blacklist gelöscht.");
+                            FactionManager.blacklistDataMap.remove(blacklistData.getId());
+                            player1.sendMessage("§8[§cBlacklist§8]§7 " + player.getName() + " hat dich von der Blacklist der " + factionData.getFullname() + " gelöscht");
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return false;
+                    }
+                }
+                player.sendMessage(Main.error + player1.getName() + " ist nicht auf der Blacklist.");
             } else {
-                player.sendMessage(Main.error + "Deine Fraktion hat keine Blacklist.");
+                player.sendMessage(Main.error + "Syntax-Fehler: /blacklist add [Spieler] [Kills] [Preis] [Grund]");
             }
-        } else {
-            player.sendMessage(Main.error_nopermission);
         }
         return false;
     }
