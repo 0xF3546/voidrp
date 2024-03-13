c5de345e7173859f4523021ca1d253bb56e1378c
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-03-13 10:34:13 +0100

    fix shout

--- src/main/java/de/polo/voidroleplay/commands/ShoutCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/ShoutCommand.java
@@ -11,13 +11,16 @@ import org.bukkit.entity.Player;
 
 public class ShoutCommand implements CommandExecutor {
     private final Utils utils;
-    public ShoutCommand(Utils utils) {
+    private final PlayerManager playerManager;
+    public ShoutCommand(Utils utils, PlayerManager playerManager) {
         this.utils = utils;
+        this.playerManager = playerManager;
         Main.registerCommand("shout", this);
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
+        PlayerData playerData = playerManager.getPlayerData(player);
         if (args.length < 1) {
             player.sendMessage(Main.error + "Syntax-Fehler: /shout [Nachricht]");
             return false;
@@ -25,7 +28,7 @@ public class ShoutCommand implements CommandExecutor {
         for (Player players : Bukkit.getOnlinePlayers()) {
             if (player.getLocation().distance(players.getLocation()) <= 28) {
                 int range = (int) player.getLocation().distance(players.getLocation());
-                players.sendMessage("§8[§2" + range + "§8] §f" + player.getName() + " schreit§8:§f " + utils.stringArrayToString(args) + "!");
+                players.sendMessage("§8[§c" + playerData.getLeve() + "§8] §f" + player.getName() + " schreit§8:§f " + utils.stringArrayToString(args) + "!");
             }
         }
         ChatUtils.LogMessage(utils.stringArrayToString(args), player.getUniqueId());
