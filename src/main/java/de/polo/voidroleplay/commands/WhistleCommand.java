ace96bb5596cd54e90173143343e64d8830ff1d3
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-03-13 10:35:15 +0100

    fix whistle

--- src/main/java/de/polo/voidroleplay/commands/WhistleCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/WhistleCommand.java
@@ -11,21 +11,23 @@ import org.bukkit.entity.Player;
 
 public class WhistleCommand implements CommandExecutor {
     private final Utils utils;
-    public WhistleCommand(Utils utils) {
+    private final PlayerManager playerManager;
+    public WhistleCommand(Utils utils, PlayerManager playerManager) {
         this.utils = utils;
+        this.playerManager = playerManager;
         Main.registerCommand("whistle", this);
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
+        PlayerData playerData = playerManager.getPlayerData(player);
         if (args.length < 1) {
             player.sendMessage(Main.error + "Syntax-Fehler: /whistle [Nachricht]");
             return false;
         }
         for (Player players : Bukkit.getOnlinePlayers()) {
             if (player.getLocation().distance(players.getLocation()) <= 3) {
-                int range = (int) player.getLocation().distance(players.getLocation());
-                players.sendMessage("§8[§2" + range +"§8] §8" + player.getName() + " flüstert§8:§8 " + utils.stringArrayToString(args));
+                players.sendMessage("§8[§c" + playerData.getLevel() +"§8] §8" + player.getName() + " flüstert§8:§8 " + utils.stringArrayToString(args));
             }
         }
         ChatUtils.LogMessage(utils.stringArrayToString(args), player.getUniqueId());
