366ecc170532f7bbe4cf661b17b03038a5f50f43
Mayson1337 <hampelmann361@gmail.com>
2024-04-06 11:46:11 +0200

    added subgroupchat

---
+++ src/main/java/de/polo/voidroleplay/commands/SubGroupChatCommand.java
@@ -0,0 +1,39 @@
+package de.polo.voidroleplay.commands;
+
+import de.polo.voidroleplay.Main;
+import de.polo.voidroleplay.dataStorage.PlayerData;
+import de.polo.voidroleplay.dataStorage.SubGroup;
+import de.polo.voidroleplay.utils.FactionManager;
+import de.polo.voidroleplay.utils.PlayerManager;
+import de.polo.voidroleplay.utils.Utils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SubGroupChatCommand implements CommandExecutor {
+    private final PlayerManager playerManager;
+    private final FactionManager factionManager;
+    public SubGroupChatCommand(PlayerManager playerManager, FactionManager factionManager) {
+        this.playerManager = playerManager;
+        this.factionManager = factionManager;
+        Main.registerCommand("subgroupchat", this);
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Player player = (Player) sender;
+        PlayerData playerData = playerManager.getPlayerData(player);
+        if (playerData.getSubGroupId() == 0) {
+            player.sendMessage(Main.error_nopermission);
+            return false;
+        }
+        if (args.length < 1) {
+            player.sendMessage(Main.error_nopermission + "Syntax-Fehler: /subgroupchat [Nachricht]");
+            return false;
+        }
+        SubGroup subGroup = playerData.getSubGroup();
+        factionManager.subGroups.sendMessage("§8[§f" + subGroup.getName() + "§8]§7 " + player.getName() + "§8:§7 " + Utils.stringArrayToString(args), subGroup);
+        return false;
+    }
+}
