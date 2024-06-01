f67c5ca93632cc72c9b397d57d95ccda7430000c
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-06-01 15:35:28 +0200

    rewrite farming-system

--- src/main/java/de/polo/voidroleplay/listeners/BlockBreakListener.java
+++ src/main/java/de/polo/voidroleplay/listeners/BlockBreakListener.java
@@ -4,18 +4,27 @@ import com.jeff_media.customblockdata.CustomBlockData;
 import de.polo.voidroleplay.dataStorage.PlayerData;
 import de.polo.voidroleplay.Main;
 import de.polo.voidroleplay.game.events.BreakPersistentBlockEvent;
+import de.polo.voidroleplay.utils.ItemManager;
 import de.polo.voidroleplay.utils.PlayerManager;
+import de.polo.voidroleplay.utils.Prefix;
+import de.polo.voidroleplay.utils.ServerManager;
+import de.polo.voidroleplay.utils.enums.Farmer;
+import de.polo.voidroleplay.utils.enums.RoleplayItem;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 
+import java.util.List;
+
 public class BlockBreakListener implements Listener {
     private final PlayerManager playerManager;
     private final Main.Commands commands;
+
     public BlockBreakListener(PlayerManager playerManager, Main.Commands commands) {
         this.playerManager = playerManager;
         this.commands = commands;
@@ -26,6 +35,20 @@ public class BlockBreakListener implements Listener {
         Player player = event.getPlayer();
         PlayerData playerData = playerManager.getPlayerData(player.getUniqueId());
         if (!player.getGameMode().equals(GameMode.CREATIVE)) {
+            if (!ServerManager.canDoJobs()) {
+                player.sendMessage(Prefix.ERROR + "Vor dem Restart kannst du keine Jobs ausführen, bei welchen du Blöcke ab baust.");
+                return;
+            }
+            for (Farmer farmer : Farmer.values()) {
+                if (player.getLocation().distance(farmer.getLocation()) < 50 && event.getBlock().getType() == farmer.getFarmingItem()) {
+                    event.setCancelled(true);
+                    event.getBlock().setType(Material.AIR);
+                    ItemManager.addCustomItem(player, RoleplayItem.ARAMID, 1);
+                    Main.waitSeconds(120, () -> {
+                        event.getBlock().setType(Material.DEAD_BUSH);
+                    });
+                }
+            }
             if (playerData.getVariable("job") != null) {
                 switch (playerData.getVariable("job").toString().toLowerCase()) {
                     case "holzfäller":
---
+++ src/main/java/de/polo/voidroleplay/utils/enums/Farmer.java
@@ -0,0 +1,25 @@
+package de.polo.voidroleplay.utils.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+/**
+ * @author Mayson1337
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+@AllArgsConstructor
+@Getter
+public enum Farmer {
+
+    ARAMID("Aramid", new Location(Bukkit.getWorld("World"), 290, 69, -311), Material.DEAD_BUSH, RoleplayItem.ARAMID);
+
+    private final String name;
+    private final Location location;
+    private final Material farmingItem;
+    private final RoleplayItem outputItem;
+}
