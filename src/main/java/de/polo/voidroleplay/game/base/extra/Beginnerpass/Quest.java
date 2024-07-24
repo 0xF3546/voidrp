503110a844116775769033320d1264db2a7fc6fd
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-07-24 18:18:55 +0200

    added beginner pass

---
+++ src/main/java/de/polo/voidroleplay/game/base/extra/Beginnerpass/Quest.java
@@ -0,0 +1,74 @@
+package de.polo.voidroleplay.game.base.extra.Beginnerpass;
+
+import org.bukkit.Material;
+
+/**
+ * @author Mayson1337
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Quest {
+    private int id;
+    private String name;
+    private String description;
+    private int rewardId;
+    private Material item;
+    private int reachedAt;
+
+    public Quest(int id, String name, String description, int reachedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.reachedAt = reachedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRewardId() {
+        return rewardId;
+    }
+
+    public void setRewardId(int rewardId) {
+        this.rewardId = rewardId;
+    }
+
+    public Material getItem() {
+        return item;
+    }
+
+    public void setItem(Material item) {
+        this.item = item;
+    }
+
+    public int getReachedAt() {
+        return reachedAt;
+    }
+
+    public void setReachedAt(int reachedAt) {
+        this.reachedAt = reachedAt;
+    }
+}
+
