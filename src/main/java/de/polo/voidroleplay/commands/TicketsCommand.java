383c10f13cf05576598e60fd75e822fc4ea72218
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-07-28 21:54:13 +0200

    fix ticket-list

--- src/main/java/de/polo/voidroleplay/commands/TicketsCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/TicketsCommand.java
@@ -29,6 +29,7 @@ public class TicketsCommand implements CommandExecutor {
             player.sendMessage("§6§lTicketübersicht§8:");
             for (Ticket ticket : supportManager.getTickets()) {
                 Player targetPlayer = Bukkit.getPlayer(ticket.getCreator());
+                if (targetPlayer == null) continue;
                 TextComponent message = new TextComponent("§8 ➥ §e" + targetPlayer.getName());
                 message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§e" + targetPlayer.getName() + "'s Ticket annehmen")));
                 message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/acceptsupport " + targetPlayer.getName()));
