919e9c12d61e0991de48f3b16a945e245cf964fe
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-08-07 00:12:54 +0200

    added command for secondary team kick

---
+++ src/main/java/de/polo/voidroleplay/commands/KickSecondaryTeam.java
@@ -0,0 +1,63 @@
+package de.polo.voidroleplay.commands;
+
+import de.polo.voidroleplay.Main;
+import de.polo.voidroleplay.dataStorage.PlayerData;
+import de.polo.voidroleplay.utils.PlayerManager;
+import de.polo.voidroleplay.utils.Prefix;
+import de.polo.voidroleplay.utils.TeamSpeak;
+import de.polo.voidroleplay.utils.Utils;
+import lombok.SneakyThrows;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ * @author Mayson1337
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class KickSecondaryTeam implements CommandExecutor {
+    private final PlayerManager playerManager;
+    public KickSecondaryTeam(PlayerManager playerManager) {
+        this.playerManager = playerManager;
+
+        Main.registerCommand("kicksecondaryteam", this);
+    }
+    @SneakyThrows
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Player player = (Player) sender;
+        PlayerData playerData = playerManager.getPlayerData(player);
+        if (playerData.getPermlevel() < 90) {
+            player.sendMessage(Prefix.error_nopermission);
+            return false;
+        }
+        if (args.length < 1) {
+            player.sendMessage(Prefix.ERROR + "Syntax-Fehler: /kicksecondaryteam [Spieler]");
+            return false;
+        }
+        OfflinePlayer offlinePlayer = Utils.getOfflinePlayer(args[0]);
+        if (offlinePlayer == null) {
+            player.sendMessage(Prefix.ERROR + "Spieler wurde nicht gefunden.");
+            return false;
+        }
+        if (offlinePlayer.isOnline()) {
+            PlayerData target = playerManager.getPlayerData(offlinePlayer.getUniqueId());
+            target.setSecondaryTeam(null);
+        }
+        player.sendMessage(Prefix.MAIN + "Du hast " + offlinePlayer.getName() + " aus seinem Sub-Team gekickt.");
+        Connection connection = Main.getInstance().mySQL.getConnection();
+        PreparedStatement statement = connection.prepareStatement("UPDATE players SET secondaryTeam = NULL WHERE uuid = ?");
+        statement.setString(1, offlinePlayer.getUniqueId().toString());
+        statement.execute();
+        connection.close();
+        TeamSpeak.reloadPlayer(offlinePlayer.getUniqueId());
+        return false;
+    }
+}
