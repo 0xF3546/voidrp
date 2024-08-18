5d632a8c75c5650cf3232d36d8ce4bf7061f72cc
0xF3546 <99101632+Mayson1337@users.noreply.github.com>
2024-08-18 17:40:24 +0200

    reworked member-list

--- src/main/java/de/polo/voidroleplay/commands/MemberCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/MemberCommand.java
@@ -7,6 +7,7 @@ import de.polo.voidroleplay.Main;
 import de.polo.voidroleplay.utils.FactionManager;
 import de.polo.voidroleplay.utils.PlayerManager;
 import de.polo.voidroleplay.utils.ServerManager;
+import de.polo.voidroleplay.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -14,16 +15,19 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class MemberCommand implements CommandExecutor {
     private final PlayerManager playerManager;
     private final FactionManager factionManager;
+
     public MemberCommand(PlayerManager playerManager, FactionManager factionManager) {
         this.playerManager = playerManager;
         this.factionManager = factionManager;
         Main.registerCommand("member", this);
     }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
@@ -31,29 +35,36 @@ public class MemberCommand implements CommandExecutor {
         if (playerData.getFaction() != null && !playerData.getFaction().equals("Zivilist")) {
             FactionData factionData = factionManager.getFactionData(playerData.getFaction());
             player.sendMessage("§7   ===§8[§" + factionData.getPrimaryColor() + factionData.getFullname() + "§8]§7===");
-            Map<Integer, List<OfflinePlayer>> sort = new HashMap<>();
+
+            Map<Integer, List<FactionPlayerData>> sort = new HashMap<>();
+
             for (FactionPlayerData factionPlayerData : factionManager.getFactionMember(playerData.getFaction())) {
                 if (factionPlayerData.getFaction().equals(playerData.getFaction())) {
-                    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(factionPlayerData.getUuid()));
-                    if (sort.containsKey(factionPlayerData.getFaction_grade())) {
-                        sort.get(factionPlayerData.getFaction_grade()).add(offlinePlayer);
-                    } else {
-                        sort.put(factionPlayerData.getFaction_grade(), new ArrayList<>());
-                        sort.get(factionPlayerData.getFaction_grade()).add(offlinePlayer);
-                    }
+                    int grade = factionPlayerData.getFaction_grade();
+                    sort.computeIfAbsent(grade, k -> new ArrayList<>()).add(factionPlayerData);
                 }
             }
+
+            // Display faction members by grade
             for (int value = 8; value >= 0; value--) {
                 String message = "§8 » §" + factionData.getSecondaryColor() + "[" + value + "] - " + factionManager.getRankName(playerData.getFaction(), value) + " - " + factionManager.getPaydayFromFaction(playerData.getFaction(), value) + "$/PayDay";
                 player.sendMessage(message);
-                if (sort.get(value) != null) {
-                    for (int i = 0; i < sort.get(value).size(); i++) {
-                        OfflinePlayer offlinePlayer = sort.get(value).get(i);
+
+                List<FactionPlayerData> members = sort.get(value);
+                if (members != null) {
+                    for (FactionPlayerData factionPlayerData : members) {
+                        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(factionPlayerData.getUuid()));
+                        LocalDateTime lastLogin = factionPlayerData.getLastLogin();
 
                         if (offlinePlayer.isOnline()) {
                             player.sendMessage("§8 → §a" + offlinePlayer.getName());
                         } else {
-                            player.sendMessage("§8 → §c" + offlinePlayer.getName());
+                            String lastLoginTime = Utils.localDateTimeToReadableString(lastLogin);
+                            if (playerData.getFactionGrade() >= 7) {
+                                player.sendMessage("§8 → §c" + offlinePlayer.getName() + " - (" + lastLoginTime + ")");
+                            } else {
+                                player.sendMessage("§8 → §c" + offlinePlayer.getName());
+                            }
                         }
                     }
                     sort.remove(value);
