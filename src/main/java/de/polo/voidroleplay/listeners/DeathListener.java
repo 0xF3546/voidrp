fdc02b6ee899ca64b3a744a4c0f01753350e7733
makr <celaticz@gmail.com>
2024-10-06 01:25:04 +0200

    changed adrenalinrush

--- src/main/java/de/polo/voidroleplay/listeners/DeathListener.java
+++ src/main/java/de/polo/voidroleplay/listeners/DeathListener.java
@@ -14,6 +14,7 @@ import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Item;
@@ -249,7 +250,7 @@ public class DeathListener implements Listener {
     private void triggerAdrenalineRush(Player player) {
 
         player.sendTitle("§x§0§0§F§F§E§0§l§oA§x§1§1§E§D§E§2§l§od§x§2§2§D§B§E§4§l§or§x§3§2§C§8§E§7§l§oe§x§4§3§B§6§E§9§l§on§x§5§4§A§4§E§B§l§oa§x§6§5§9§2§E§D§l§ol§x§7§6§8§0§F§0§l§oi§x§8§6§6§D§F§2§l§on§x§9§7§5§B§F§4§l§or§x§A§8§4§9§F§6§l§oa§x§B§9§3§7§F§8§l§ou§x§C§9§2§4§F§B§l§os§x§D§A§1§2§F§D§l§oc§x§E§B§0§0§F§F§l§oh", "§7Du bist für 5 Sekunden im Adrenalinrausch!", 10, 20, 10);
-        player.playSound(player.getLocation(), "ENTITY_ENDER_DRAGON_GROWL", 1.0f, 1.0f);
+        player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f);
 
         AttributeInstance healthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
         if (healthAttribute != null) {
