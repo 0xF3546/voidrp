30ac5fef3346e0132d18757d80810b9c8b965f75
TheodoriusStrattmannius <99101632+0xF3546@users.noreply.github.com>
2024-12-11 13:41:17 +0100

    added loyaltybonus to stats

--- src/main/java/de/polo/voidroleplay/commands/StatsCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/StatsCommand.java
@@ -43,6 +43,7 @@ public class StatsCommand implements CommandExecutor {
         PlaytimeReward playtimeReward = playerManager.getPlaytimeReward(playerData.getRewardId());
         player.sendMessage("§8 - §6Spielzeitbelohnung§8: §c" + playtimeReward.getDisplayName() + " §8- §c" + playerData.getRewardTime() + "h verbleibend" + (playtimeReward.isPremiumOnly() ? "§8[§6Premium§8]" : ""));
         player.sendMessage("§8 - §6Inventar§8: §c" + playerData.getInventory().getItems().size() + "/" + playerData.getInventory().getSize());
+        player.sendMessage("§8 - §6Treuepunkte§8:§c " + playerData.getLoyaltyBonus());
         return false;
     }
 }
