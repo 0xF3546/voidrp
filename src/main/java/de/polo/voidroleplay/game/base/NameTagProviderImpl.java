ffa48734736e8e4d983946764c523096c23e04ed
Erik <the.real.splatcrafter@gmail.com>
2024-12-13 14:26:47 +0100

    Add CustomTabAPI and deprecate NameTagProviderImpl
    Introduced CustomTabAPI for managing player ranks, tab headers, and footers, with streamlined functionality and scoreboards. Deprecated NameTagProviderImpl for future removal, aligning with the new API structure.

--- src/main/java/de/polo/voidroleplay/game/base/NameTagProviderImpl.java
+++ src/main/java/de/polo/voidroleplay/game/base/NameTagProviderImpl.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import java.util.Collection;
 import java.util.List;
 
+@Deprecated(forRemoval = true)
 public class NameTagProviderImpl implements INameTagProvider {
 
     private final ProtocolManager protocolManager;
