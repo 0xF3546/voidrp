977bb24921315ca646a7a818f7bed04821552dbb
vansencool <vansencool@gmail.com>
2024-12-15 12:36:04 +0530

    Patch

--- src/main/java/de/polo/voidroleplay/commands/WantedCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/WantedCommand.java
@@ -7,16 +7,14 @@ import de.polo.voidroleplay.storage.WantedReason;
 import de.polo.voidroleplay.utils.Prefix;
 import de.polo.voidroleplay.utils.Utils;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 public class WantedCommand implements CommandExecutor {
     private final PlayerManager playerManager;
@@ -37,21 +35,21 @@ public class WantedCommand implements CommandExecutor {
             return false;
         }
         if (playerData.getFaction().equals("FBI") || playerData.getFaction().equals("Polizei")) {
-            CompletableFuture.runAsync(() -> {
-                try (Statement statement = Main.getInstance().getMySQL().getStatement();
-                     ResultSet result = statement.executeQuery("SELECT * FROM player_wanteds")) {
+            Main.getInstance().getMySQL().queryThreaded("SELECT * FROM player_wanteds").thenAccept(result -> {
+                try {
                     int i = 0;
-                    while (result.next()) {
+                    while (result.next()) { // Durchläuft alle Zeilen im ResultSet
                         System.out.println("Found: " + result);
-                        WantedReason wantedReason = utils.staatUtil.getWantedReason(result.getInt("wantedId"));
-                        Player player1 = Bukkit.getPlayer(UUID.fromString(result.getString("uuid")));
-                        if (player1 == null) continue;
+                        WantedReason wantedReason = utils.staatUtil.getWantedReason(result.resultSet().getInt("wantedId"));
+                        OfflinePlayer player1 = Bukkit.getOfflinePlayer(UUID.fromString(result.resultSet().getString("uuid")));
+                        if (!player1.isOnline()) continue; // Nur anzeigen, wenn der Spieler online ist
                         i++;
                         player.sendMessage("§cGesucht! §8- §9" + player1.getName() + " §8-§9 " + wantedReason.getReason() + " §8-§9 " + wantedReason.getWanted() + " Wanteds");
                     }
                     if (i == 0) {
                         player.sendMessage("§9Es steht niemand auf der Fahndungsliste.");
                     }
+                    result.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
