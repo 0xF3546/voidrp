fe0aaab902102f4df8c404f880e4a1b5a16bbf48
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2024-12-17 02:05:20 +0100

    fixed logic issue in time check

--- src/main/java/de/polo/voidroleplay/commands/PremiumCommand.java
+++ src/main/java/de/polo/voidroleplay/commands/PremiumCommand.java
@@ -24,15 +24,24 @@ public class PremiumCommand extends CommandBase {
 
     @Override
     public void execute(@NotNull Player player, @NotNull PlayerData playerData, @NotNull String[] args) throws Exception {
-        if (playerData.getLastPremiumBonus() != null && playerData.getLastPremiumBonus().plusWeeks(1).isBefore(Utils.getTime())) {
-            player.sendMessage(Component.text(Prefix.ERROR + "Du kannst aktuell deine Belohnung nicht abholen."));
+        if (playerData.getLastPremiumBonus() != null
+                && playerData.getLastPremiumBonus().plusWeeks(1).isAfter(Utils.getTime())) {
+            player.sendMessage(Component.text(Prefix.ERROR + "Du kannst deine Belohnung erst nächste Woche wieder abholen."));
             return;
         }
+
         Main.getInstance().getWeaponManager().giveWeaponToCabinet(player, Weapon.MARKSMAN, 20, 1);
         playerData.addBankMoney(1000, "Premium-Belohnung");
         Main.getInstance().playerManager.addEXPBoost(player, 1);
 
         playerData.setLastPremiumBonus(Utils.getTime());
-        Main.getInstance().getMySQL().updateAsync("UPDATE players SET lastPremiumBonus = ? WHERE uuid = ?", Utils.getTime().toString(), player.getUniqueId().toString());
+        Main.getInstance().getMySQL().updateAsync(
+                "UPDATE players SET lastPremiumBonus = ? WHERE uuid = ?",
+                Utils.getTime().toString(),
+                player.getUniqueId().toString()
+        );
+
+        player.sendMessage(Component.text(Prefix.MAIN + "Du hast deinen Bonus erfolgreich abgeholt."));
     }
+
 }
