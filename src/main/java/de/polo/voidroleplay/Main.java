76c858426ee4383c144e65155c5366ae72f629e5
EinsTify <einstify@gmail.com>
2024-12-23 19:28:04 +0100

    Fixed springen mit cuffs

--- src/main/java/de/polo/voidroleplay/Main.java
+++ src/main/java/de/polo/voidroleplay/Main.java
@@ -226,6 +226,7 @@ public final class Main extends JavaPlugin {
         new ExplosionListener();
         new ConsumeListener(playerManager);
         new EntityToggleGlideListener();
+        new JumpListener(playerManager);
 
     }
 
