0bc8828da52909bc406a44acc45bfc3d9c6b8e0a
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2024-12-31 02:15:39 +0100

    Revert "cleanup"
    This reverts commit 89aace2ee8e6480d08d94f6500471213062262bd.

--- src/main/java/de/polo/voidroleplay/listeners/PacketSendListener.java
+++ src/main/java/de/polo/voidroleplay/listeners/PacketSendListener.java
@@ -4,6 +4,7 @@ import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerInfoUpdate;
+import de.polo.voidroleplay.Main;
 import de.polo.voidroleplay.manager.FactionManager;
 import de.polo.voidroleplay.manager.PlayerManager;
 import de.polo.voidroleplay.storage.BlacklistData;
@@ -16,8 +17,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.text.MessageFormat;
-import java.util.Objects;
-import java.util.UUID;
 
 import static de.polo.voidroleplay.Main.utils;
 
@@ -26,69 +25,58 @@ public class PacketSendListener implements PacketListener {
     private final PlayerManager playerManager;
     private final FactionManager factionManager;
 
-    public PacketSendListener(PlayerManager playerManager, FactionManager factionManager) {
+    public PacketSendListener(PlayerManager playerManager, FactionManager factionManager){
         this.playerManager = playerManager;
         this.factionManager = factionManager;
     }
 
     @Override
-    public void onPacketSend(PacketSendEvent event) {
-        if (event.getPacketType() != PacketType.Play.Server.PLAYER_INFO_UPDATE) {
-            return;
-        }
-
-        Player target = event.getPlayer();
-        WrapperPlayServerPlayerInfoUpdate playerInfoUpdate = new WrapperPlayServerPlayerInfoUpdate(event);
-
-        for (var entry : playerInfoUpdate.getEntries()) {
-            Player sender = Bukkit.getPlayer(entry.getProfileId());
-            if (sender == null || target.equals(sender)) continue;
-
-            PlayerData targetData = playerManager.getPlayerData(target.getUniqueId());
-            PlayerData senderData = playerManager.getPlayerData(sender.getUniqueId());
-
-            if (targetData == null || senderData == null) continue;
-            processRelationship(entry, sender, targetData, senderData);
-
-            FactionData factionData = factionManager.getFactionData(targetData.getFaction());
-            if (factionData == null) continue;
-
-            processGoodFaction(target, sender, entry, targetData, senderData);
-            processBadFaction(entry, senderData, targetData, sender);
-            processSameFaction(entry, sender, targetData, factionData);
-        }
-    }
-
-    private void processGoodFaction(Player target, Player sender, WrapperPlayServerPlayerInfoUpdate.PlayerInfo entry, PlayerData targetData, PlayerData senderData) {
-        if (factionManager.isPlayerInGoodFaction(target) && !"Medic".equalsIgnoreCase(targetData.getFaction())) {
-            if (senderData.getWanted() != null) {
-                WantedReason wantedReason = utils.staatUtil.getWantedReason(senderData.getWanted().getWantedId());
-                if (wantedReason != null) {
-                    int wantedLevel = wantedReason.getWanted();
-                    NamedTextColor color = wantedLevel >= 50 ? NamedTextColor.DARK_RED : NamedTextColor.RED;
-                    entry.setDisplayName(Component.text(MessageFormat.format("{0} {1} WPS", sender.getName(), wantedLevel)).color(color));
+    public void onPacketSend(PacketSendEvent event){
+        if(event.getPacketType() == PacketType.Play.Server.PLAYER_INFO_UPDATE){
+            Player target = event.getPlayer();
+            WrapperPlayServerPlayerInfoUpdate playerInfoUpdate = new WrapperPlayServerPlayerInfoUpdate(event);
+            for(var entry : playerInfoUpdate.getEntries()){
+                Player sender = Bukkit.getPlayer(entry.getProfileId());
+                if(target != sender) {
+                    PlayerData targetData = playerManager.getPlayerData(target.getUniqueId());
+                    FactionData factionData = factionManager.getFactionData(targetData.getFaction());
+                    PlayerData senderData = playerManager.getPlayerData(sender.getUniqueId());
+                    //WPS für Goodfraks
+                    if(factionManager.isPlayerInGoodFaction(target)){
+                        if(!targetData.getFaction().equalsIgnoreCase("Medic")){
+                            if(senderData.getWanted() != null){
+                                WantedReason wantedReason = utils.staatUtil.getWantedReason(senderData.getWanted().getWantedId());
+                                if(wantedReason.getWanted() >= 55){
+                                    entry.setDisplayName(Component.text(MessageFormat.format("{0} {1} WPS", sender.getName(), wantedReason.getWanted())).color(NamedTextColor.DARK_RED));
+                                }
+                                else{
+                                    entry.setDisplayName(Component.text(MessageFormat.format("{0} {1} WPS", sender.getName(), wantedReason.getWanted())).color(NamedTextColor.RED   ));
+                                }
+                            }
+                        }
+                    }
+                    //Blacklists für Badfraks
+                    else{
+                        for(BlacklistData blacklistData : factionManager.getBlacklists()){
+                            //maybe always false IDE zeigt irwas an sieht aber wie fehler aus
+                            if(blacklistData.getUuid().equals(senderData.getUuid()) && blacklistData.getFaction().equals(targetData.getFaction())){
+                                entry.setDisplayName(Component.text(sender.getName()).color(NamedTextColor.DARK_RED));
+                            }
+                        }
+                    }
+                    //Wenn Spieler in gleicher Frak dann Namen in Primary Color Anzeigen
+                    if(targetData.getFaction().equals(senderData.getFaction())) {
+                        entry.setDisplayName(Component.text("§" + factionData.getPrimaryColor() +sender.getName()));
+                    }
+                    //Verlobt / Verheiratet?
+                    if(!targetData.getRelationShip().isEmpty() && !senderData.getRelationShip().isEmpty()){
+                        if(targetData.getRelationShip().get(sender.getUniqueId().toString()) != null){
+                            entry.setDisplayName(Component.text(sender.getName()).color(NamedTextColor.LIGHT_PURPLE));
+                        }
+                    }
                 }
             }
         }
     }
 
-    private void processBadFaction(WrapperPlayServerPlayerInfoUpdate.PlayerInfo entry, PlayerData senderData, PlayerData targetData, Player sender) {
-        for (BlacklistData blacklistData : factionManager.getBlacklists()) {
-            if (blacklistData == null) continue;
-            if (UUID.fromString(blacklistData.getUuid()) != senderData.getUuid()
-                    || !blacklistData.getFaction().equalsIgnoreCase(targetData.getFaction())) continue;
-            entry.setDisplayName(Component.text(sender.getName()).color(NamedTextColor.DARK_RED));
-        }
-    }
-
-    private void processSameFaction(WrapperPlayServerPlayerInfoUpdate.PlayerInfo entry, Player sender, PlayerData targetData, FactionData factionData) {
-        if (!targetData.getFaction().equalsIgnoreCase(factionData.getName())) return;
-        entry.setDisplayName(Component.text("§" + factionData.getPrimaryColor() + sender.getName()));
-    }
-
-    private void processRelationship(WrapperPlayServerPlayerInfoUpdate.PlayerInfo entry, Player sender, PlayerData targetData, PlayerData senderData) {
-        if (targetData.getRelationShip().isEmpty() || senderData.getRelationShip().isEmpty()) return;
-        if (!targetData.getRelationShip().containsKey(sender.getUniqueId().toString())) return;
-        entry.setDisplayName(Component.text(sender.getName()).color(NamedTextColor.LIGHT_PURPLE));
-    }
 }
