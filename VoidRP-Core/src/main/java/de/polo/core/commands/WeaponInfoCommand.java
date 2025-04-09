b453fdaf993a269537c7f08c800f874245b2d908
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-04-09 11:26:41 +0200

    changed weaponinfo-layout

--- VoidRP-Core/src/main/java/de/polo/core/commands/WeaponInfoCommand.java
+++ VoidRP-Core/src/main/java/de/polo/core/commands/WeaponInfoCommand.java
@@ -5,6 +5,7 @@ import de.polo.core.player.entities.PlayerData;
 import de.polo.core.storage.PlayerWeapon;
 import de.polo.core.player.services.impl.PlayerManager;
 import de.polo.core.utils.enums.Weapon;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -24,6 +25,7 @@ public class WeaponInfoCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player player = (Player) sender;
         PlayerData playerData = playerManager.getPlayerData(player);
+        player.sendMessage(Component.text("§7   ===§8[§2Waffenschrank§8]===§7"));
         for (PlayerWeapon playerWeapon : playerData.getWeapons()) {
             Weapon weapon = playerWeapon.getWeapon();
             player.sendMessage("§8 - §b" + weapon.getClearName() + "§8:§7 A: " + playerWeapon.getAmmo() + " V: " + playerWeapon.getWear());
