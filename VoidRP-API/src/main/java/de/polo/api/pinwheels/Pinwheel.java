b1c276a79491a4479b4fe0ee0ea6e095a7ebcca9
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-04-11 13:51:43 +0200

    Add Pinwheel entities

---
+++ VoidRP-API/src/main/java/de/polo/api/pinwheels/Pinwheel.java
@@ -0,0 +1,13 @@
+package de.polo.api.pinwheels;
+
+import org.bukkit.Location;
+
+/**
+ * @author Mayson1337
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public interface Pinwheel {
+    Location location();
+    String name();
+}
---
+++ VoidRP-Core/src/main/java/de/polo/core/pinwheels/entities/CorePinwheel.java
@@ -0,0 +1,13 @@
+package de.polo.core.pinwheels.entities;
+
+import de.polo.api.pinwheels.Pinwheel;
+import lombok.Getter;
+import org.bukkit.Location;
+
+/**
+ * @author Mayson1337
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public record CorePinwheel(@Getter Location location, @Getter String name) implements Pinwheel {
+}
