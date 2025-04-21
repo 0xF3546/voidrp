4671f6d4f87d147d7fcaa5dff6e444ab9955dc08
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-04-21 14:33:27 +0200

    Add BuildMode Info

--- VoidRP-Core/src/main/java/de/polo/core/listeners/GameModeChangeEvent.java
+++ VoidRP-Core/src/main/java/de/polo/core/listeners/GameModeChangeEvent.java
@@ -4,7 +4,11 @@ import de.polo.core.Main;
 import de.polo.core.player.entities.PlayerData;
 import de.polo.core.utils.Utils;
 import de.polo.core.utils.Event;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,11 +27,14 @@ public class GameModeChangeEvent implements Listener {
         // ISSUE VRP-10004: fixed by adding null check
         if (playerData == null) return;
         if (event.getNewGameMode() == GameMode.SURVIVAL) {
+            playerData.removeBossBar("buildMode");
             if (playerData.getVariable("inventory::build") == null) return;
             player.getInventory().setContents(playerData.getVariable("inventory::build"));
             playerData.setVariable("inventory::build", null);
         }
         if (event.getNewGameMode() == GameMode.CREATIVE) {
+            BossBar bossBar = Bukkit.createBossBar("§8⚒ §2Du befindest dich im Baumodus §8⚒", BarColor.GREEN, BarStyle.SOLID);
+            playerData.setBossBar("buildMode", bossBar);
             playerData.setVariable("inventory::build", player.getInventory().getContents());
             Utils.Tablist.setTablist(player, "§8[§2GM§8]");
             player.getInventory().clear();
