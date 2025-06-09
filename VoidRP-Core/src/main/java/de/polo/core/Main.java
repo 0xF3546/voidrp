92095c2918e231cae26068e369192671eec74703
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-06-09 12:20:12 +0200

    auto clear online players

--- VoidRP-Core/src/main/java/de/polo/core/Main.java
+++ VoidRP-Core/src/main/java/de/polo/core/Main.java
@@ -191,6 +191,7 @@ public final class Main extends JavaPlugin implements Server {
         getLogger().info("§VOIDROLEPLAY ROLEPLAY STARTED.");
         try {
             Statement statement = coreDatabase.getStatement();
+            statement.execute("DELETE FROM players_online");
             statement.execute("DELETE FROM bank_logs WHERE datum < DATE_SUB(NOW(), INTERVAL 7 DAY)");
             statement.execute("DELETE FROM money_logs WHERE datum < DATE_SUB(NOW(), INTERVAL 7 DAY)");
             statement.execute("DELETE FROM players_online_log WHERE time < DATE_SUB(NOW(), INTERVAL 7 DAY)");
